5191f6b5afe77174063fe900713d690732151a09
Mitch Merrell <themitchguy@gmail.com>
2026-03-29 20:35:49 -0700

    updated client message loop

--- MyClient.java
+++ MyClient.java
@@ -4,14 +4,17 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class MyClient implements Runnable {
+
+    private String name;
     private int port;
     private Socket socket;
     private ObjectOutputStream outgoingMessageStream; // message that the client wants to send
     private ObjectInputStream serverResponseStream; // input from server
 
-    public MyClient(String host, int port) throws IOException {
+    public MyClient(String host, int port, String name) throws IOException {
         this.port = port;
         this.socket = new Socket(host, port);
+        this.name = name;
     }
 
     public void run() {
@@ -19,18 +22,20 @@ public class MyClient implements Runnable {
             outgoingMessageStream = new ObjectOutputStream(socket.getOutputStream());
             serverResponseStream = new ObjectInputStream(socket.getInputStream());
 
-            int responsesReceived = 0;
+            int messagesSent = 0;
 
-            while(responsesReceived < 5) {
-                outgoingMessageStream.writeObject(new Message("outgoing client message #" + (responsesReceived + 1)));
+            while(messagesSent < 5) {
+                outgoingMessageStream.writeObject(new Message("outgoing client message #" + (messagesSent + 1) + " from " + name));
                 outgoingMessageStream.flush();
 
                 Message incomingMessage = null;
-                while (incomingMessage == null) {
+
+                do { 
                     incomingMessage = (Message) serverResponseStream.readObject();
-                }
-                System.out.println(incomingMessage.getMessage());
-                responsesReceived++;
+                    System.out.println(name + " receiving " + incomingMessage.getMessage());
+                } while (serverResponseStream.available() != 0);
+
+                messagesSent++;
             }
 
             serverResponseStream.close();

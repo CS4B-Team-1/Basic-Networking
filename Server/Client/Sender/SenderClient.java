0397fe03e0578e18abfba94ddcc1bdbf9fdd35cf
Mitch Merrell <themitchguy@gmail.com>
2026-04-04 19:49:00 -0700

    updated with sender client

--- Server/Client/Sender/SenderClient.java
+++ Server/Client/Sender/SenderClient.java
@@ -1,4 +1,4 @@
-package Server.Client.Receiver;
+package Server.Client.Sender;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -6,47 +6,54 @@ import java.net.Socket;
 
 import Server.Message;
 
-public class SenderClient1 {
+public class SenderClient{
     private int port;
     private String name;
     private Socket socket;
     private ObjectOutputStream outgoingMessageStream; 
 
 
-    public SenderClient1(int p, String n) throws IOException {
+    public SenderClient(int p, String n) throws IOException {
         this.port = p;
         this.name = n;
 
         //local host is hard coded
         this.socket = new Socket("localhost", port);
         this.outgoingMessageStream = new ObjectOutputStream(socket.getOutputStream());
-        
     }
 
+
+    
+        /*
+            SENDER CLIENT: ONLY SENDS MESSAGES TO THE SERVER
+        */
     public static void main(String[] args) {
 
-        /*
-            SENDER CLIENT 1:
-        */
 
         int messagesSent = 0;
         try {
-            SenderClient1 senderClient1 = new SenderClient1(8080, "Sender Client 1");
+            //Open socket connection
+            SenderClient senderClient = new SenderClient(8080, "Sender Client");
 
+            //infinite loop to send messages every second
             while(true) {
-                senderClient1.sendMessage(new Message("outgoing client message #" + (messagesSent + 1) + " from " + senderClient1.name));
+                senderClient.sendMessage(new Message("outgoing client message #" + (messagesSent + 1) + " from " + senderClient.name));
 
+                Thread.sleep(1000);
                 messagesSent++;
             } 
 
-        } catch (IOException e) {
+            //Call methods to close the threads/Sockets/IO streams
+            // senderClient.socket.close();
+
+        } catch (IOException e ) {
             System.out.println("Exception thrown while read/write of incoming/outgoing MyClient Message: " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println("Exception thrown while sleeping: " + e.getMessage());
         }
         
         
-        //Call methods to close the threads/Sockets/IO streams
-        // SenderClient.outgoingMessageStream.close();
-        // this.socket.close();
+
 
     }
 
@@ -56,6 +63,5 @@ public class SenderClient1 {
         outgoingMessageStream.writeObject(message);
         outgoingMessageStream.flush();
     }
-
 
 }

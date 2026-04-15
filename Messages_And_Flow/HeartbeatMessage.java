62bba6094d9570c70ca48aa24f250e1590f933d4
abakhtiari1 <abakhtiari1@saddleback.edu>
2026-04-14 18:02:01 -0700

    Update HeartbeatMessage.java

--- Messages_And_Flow/HeartbeatMessage.java
+++ Messages_And_Flow/HeartbeatMessage.java
@@ -3,7 +3,7 @@ package Messages_And_Flow;
 public class HeartbeatMessage extends Message {
     private HeartbeatPayload payload;
 
-    public ErrorMessage(String messageId, String senderId, String channel, String timestamp, HeartbeatPayload payload) {
+    public HeartbeatMessage(String messageId, String senderId, String channel, String timestamp, HeartbeatPayload payload) {
         super(messageId, senderId, channel, timestamp, "HEARTBEAT");
         this.payload = payload;
     }
